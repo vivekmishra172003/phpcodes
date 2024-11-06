46b2e82cfbcbf51bbfcdd2922472bbb1fa41d338
Vivek Mishra <92047197+vivekmishra172003@users.noreply.github.com>
2024-11-06 19:17:38 +0530

    Add files via upload

---
+++ switch.php
@@ -0,0 +1,16 @@
+<?php
+echo "hello etp";
+define("NUM",1);
+$var = 2;
+switch (NUM){
+    case 1:
+        echo"Today";
+        break;
+    case 2:
+        echo"Tomorrow";
+        break;
+    default:
+        echo"Invalid";
+        break;
+}
+?>
